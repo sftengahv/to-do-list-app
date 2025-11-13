b231bd5976863f6c38d3d1aaf477b36c61162fa3
sftengahv! <armandohernandezv@icloud.com>
2025-11-12 20:36:20 -0500

    1.9

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+ 
+  $('#button').on('click', function() {
+    const toAdd = $('#taskInput').val().trim();
+
+    if (toAdd !== '') {
+      $('ol').append('<li>' + toAdd + ' <button class="delete">X</button></li>');
+      $('#taskInput').val('');
+    }
+  });
+
+  
+  $('ol').on('click', 'li', function() {
+    $(this).toggleClass('strike');
+  });
+
+  
+  $('ol').on('click', '.delete', function(e) {
+    e.stopPropagation();
+    $(this).parent().fadeOut(300, function() {
+      $(this).remove();
+    });
+  });
+
+  
+  $('ol').on('mouseenter', 'li', function() {
+    $(this).find('.delete').show();
+  });
+
+  $('ol').on('mouseleave', 'li', function() {
+    $(this).find('.delete').hide();
+  });
+});
